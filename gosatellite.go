c303123bdd6f5fb3bf8875113fbb34bba6194144
Adam Robinson <adarobin@umich.edu>
2020-02-20 14:03:53 -0500

    implemented org create,delete, and get

---
+++ gosatellite.go
@@ -0,0 +1,182 @@
+package gosatellite
+
+import (
+	"bytes"
+	"context"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+const (
+	libraryVersion = "0.1.0"
+	defaultBaseURL = "https://satellite.example.com"
+	userAgent      = "gosatellite/" + libraryVersion
+	mediaType      = "application/json"
+)
+
+// Config defines the configuration needed to connect to the
+// IBM Spectrum Protect Operations Center server
+type Config struct {
+	Username      string
+	Password      string
+	SatelliteHost string
+	SSLVerify     bool
+}
+
+// Client is the API client for IBM Spectrum Protect Operations Center
+type Client struct {
+	client *http.Client
+
+	BaseURL *url.URL
+
+	UserAgent string
+
+	Organizations Organizations
+
+	Config *Config
+
+	// Optional function called after every successful request made to the DO APIs
+	onRequestCompleted RequestCompletionCallback
+}
+
+// RequestCompletionCallback defines the type of the request callback function
+type RequestCompletionCallback func(*http.Request, *http.Response)
+
+// NewClient returns a new IBM Spectrum Protect Operations Center REST API client
+func NewClient(config *Config) (*Client, error) {
+	defaultBaseURL := "https://" + config.SatelliteHost
+	baseURL, err := url.Parse(defaultBaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	if !config.SSLVerify {
+		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	}
+
+	c := &Client{client: http.DefaultClient, BaseURL: baseURL, UserAgent: userAgent, Config: config}
+	c.Organizations = &OrganizationsOp{client: c}
+
+	return c, nil
+}
+
+// NewRequest creates an API request. A relative URL can be provided in urlStr, which will be resolved to the
+// BaseURL of the Client. Relative URLS should always be specified without a preceding slash. If specified, the
+// value pointed to by body is JSON encoded and included in as the request body.
+func (c *Client) NewRequest(ctx context.Context, method, urlStr string, body interface{}) (*http.Request, error) {
+	rel, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	u := c.BaseURL.ResolveReference(rel)
+
+	buf := new(bytes.Buffer)
+	if body != nil {
+		err = json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	req.SetBasicAuth(c.Config.Username, c.Config.Password)
+
+	req.Header.Add("Content-Type", mediaType)
+	req.Header.Add("Accept", mediaType)
+	req.Header.Add("User-Agent", c.UserAgent)
+
+	return req, nil
+}
+
+// Do sends an API request and returns the API response. The API response is JSON decoded and stored in the value
+// pointed to by v, or returned as an error if an API error has occurred. If v implements the io.Writer interface,
+// the raw response will be written to v, without attempting to decode it.
+func (c *Client) Do(ctx context.Context, req *http.Request, v interface{}) (*http.Response, error) {
+	resp, err := DoRequestWithClient(ctx, c.client, req)
+	if err != nil {
+		return nil, err
+	}
+	if c.onRequestCompleted != nil {
+		c.onRequestCompleted(req, resp)
+	}
+
+	defer func() {
+		if rerr := resp.Body.Close(); err == nil {
+			err = rerr
+		}
+	}()
+
+	err = CheckResponse(resp)
+	if err != nil {
+		return resp, err
+	}
+
+	if v != nil {
+		if w, ok := v.(io.Writer); ok {
+			_, err = io.Copy(w, resp.Body)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			err = json.NewDecoder(resp.Body).Decode(v)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return resp, err
+}
+
+// DoRequestWithClient submits an HTTP request using the specified client.
+func DoRequestWithClient(
+	ctx context.Context,
+	client *http.Client,
+	req *http.Request) (*http.Response, error) {
+	req = req.WithContext(ctx)
+	return client.Do(req)
+}
+
+// CheckResponse checks the API response for errors, and returns them if present. A response is considered an
+// error if it has a status code outside the 200 range. API error responses are expected to have either no response
+// body, or a JSON response body that maps to ErrorResponse. Any other response body will be silently ignored.
+func CheckResponse(r *http.Response) error {
+	if c := r.StatusCode; c >= 200 && c <= 299 {
+		return nil
+	}
+
+	errorResponse := &ErrorResponse{Response: r}
+	data, err := ioutil.ReadAll(r.Body)
+	if err == nil && len(data) > 0 {
+		err := json.Unmarshal(data, errorResponse)
+		if err != nil {
+			errorResponse.Message = string(data)
+		}
+	}
+
+	return errorResponse
+}
+
+func (r *ErrorResponse) Error() string {
+	return fmt.Sprintf("%v %v: %d %v",
+		r.Response.Request.Method, r.Response.Request.URL, r.Response.StatusCode, r.Message)
+}
+
+// An ErrorResponse reports the error caused by an API request
+type ErrorResponse struct {
+	// HTTP response that caused this error
+	Response *http.Response
+
+	// Error message
+	Message string `json:"message"`
+}
---
+++ organizations.go
@@ -0,0 +1,289 @@
+package gosatellite
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+)
+
+const katelloBasePath = "/katello/api"
+
+type orgCompRes struct {
+	ID                   *int    `json:"id"`
+	Name                 *string `json:"name"`
+	Provider             *string `json:"provider"`
+	ProviderFriendlyName *string `json:"provider_friendly_name"`
+}
+
+type orgOwnerDetails struct {
+	AutobindDisabled      *bool                       `json:"autobindDisabled"`
+	ContentAccessMode     *string                     `json:"contentAccessMode"`
+	ContentAccessModeList *string                     `json:"contentAccessModeList"`
+	ContentPrefix         *string                     `json:"contentPrefix"`
+	Created               *string                     `json:"created"`
+	DefaultServiceLevel   *string                     `json:"defaultServiceLevel"`
+	DisplayName           *string                     `json:"displayName"`
+	Href                  *string                     `json:"href"`
+	ID                    *string                     `json:"id"`
+	Key                   *string                     `json:"key"`
+	LastRefreshed         *string                     `json:"lastRefreshed"`
+	LogLevel              *string                     `json:"logLevel"`
+	ParentOwner           *string                     `json:"parentOwner"`
+	Updated               *string                     `json:"updated"`
+	UpstreamConsumer      []*orgOwnerUpstreamConsumer `json:"upstreamConsumer"`
+	VirtWho               *bool                       `json:"virt_who"`
+}
+
+type orgOwnerUpstreamConsumer struct {
+	APIURL            *string                         `json:"apiUrl"`
+	ContentAccessMode *string                         `json:"contentAccessMode"`
+	Created           *string                         `json:"created"`
+	ID                *string                         `json:"id"`
+	IDCert            *orgOwnerUpstreamConsumerIDCert `json:"idCert"`
+	Name              *string                         `json:"name"`
+	OwnerID           *string                         `json:"ownerId"`
+	Type              *struct {
+		ID       *string `json:"id"`
+		Label    *string `json:"label"`
+		Manifest *bool   `json:"manifest"`
+	} `json:"type"`
+	Updated *string `json:"updated"`
+	UUID    *string `json:"uuid"`
+	WebURL  *string `json:"webUrl"`
+}
+
+type orgOwnerUpstreamConsumerIDCert struct {
+	Cert    *string `json:"cert"`
+	Created *string `json:"created"`
+	ID      *string `json:"id"`
+	Key     *string `json:"key"`
+	Serial  *struct {
+		Collected  *bool   `json:"collected"`
+		Created    *string `json:"created"`
+		Expiration *string `json:"expiration"`
+		ID         *int    `json:"id"`
+		Revoked    *bool   `json:"revoked"`
+		Serial     *int    `json:"serial"`
+		Updated    *string `json:"updated"`
+	} `json:"serial"`
+	Updated *string `json:"updated"`
+}
+
+type orgParameter struct {
+	CreatedAt     *string `json:"created_at"`
+	ID            *int    `json:"id"`
+	Name          *string `json:"name"`
+	ParameterType *string `json:"parameter_type"`
+	Priority      *int    `json:"priority"`
+	UpdatedAt     *string `json:"updated_at"`
+	Value         *string `json:"value"`
+}
+
+type orgPtables struct {
+	CreatedAt *string `json:"created_at"`
+	ID        *int    `json:"id"`
+	Name      *string `json:"name"`
+	OsFamily  *string `json:"os_family"`
+	UpdatedAt *string `json:"updated_at"`
+}
+
+type orgReference struct {
+	Description *string `json:"description"`
+	ID          *int    `json:"id"`
+	Name        *string `json:"name"`
+	Title       *string `json:"title"`
+}
+
+type orgShortRef struct {
+	ID   *int    `json:"id"`
+	Name *string `json:"name"`
+}
+
+type orgSmartProxy struct {
+	ID   *int    `json:"id"`
+	Name *string `json:"name"`
+	URL  *string `json:"url"`
+}
+
+type orgSubnet struct {
+	Description    *string `json:"description"`
+	ID             *int    `json:"id"`
+	Name           *string `json:"name"`
+	NetworkAddress *string `json:"network_address"`
+}
+
+type orgTemplate struct {
+	ID               *string `json:"id"`
+	Name             *string `json:"name"`
+	TemplateKindID   *int    `json:"template_kind_id"`
+	TemplateKindName *string `json:"template_kind_name"`
+}
+
+type orgUser struct {
+	Description *string `json:"description"`
+	ID          *int    `json:"id"`
+	Login       *string `json:"login"`
+}
+
+// Organization defines model for an Organization.
+type Organization struct {
+	Ancestry              *string            `json:"ancestry"`
+	ComputeResources      *[]orgCompRes      `json:"compute_resources"`
+	ConfigTemplates       *[]orgTemplate     `json:"config_templates"`
+	CreatedAt             *string            `json:"created_at"`
+	DefaultContentViewID  *int               `json:"default_content_view_id"`
+	Description           *string            `json:"description"`
+	Domains               *[]orgShortRef     `json:"domains"`
+	Environments          *[]orgShortRef     `json:"environments"`
+	HostGroups            *[]orgReference    `json:"hostgroups"`
+	HostsCount            *int               `json:"hosts_count"`
+	ID                    *int               `json:"id"`
+	Label                 *string            `json:"label"`
+	LibraryID             *int               `json:"library_id"`
+	Locations             *[]orgReference    `json:"locations"`
+	Media                 *[]orgShortRef     `json:"media"`
+	Name                  *string            `json:"name"`
+	OwnerDetails          *[]orgOwnerDetails `json:"owner_details"`
+	Parameters            *[]orgParameter    `json:"parameters"`
+	ParentID              *int               `json:"parent_id"`
+	ParentName            *string            `json:"parent_name"`
+	ProvisioningTemplates *[]orgTemplate     `json:"provisioning_templates"`
+	Ptables               *[]orgPtables      `json:"ptables"`
+	//Realms                *[]orgReference  `json:"realms"`
+	RedHatRepositoryURL *string `json:"redhat_repository_url"`
+	//SelectAllTypes      *[]orgReference  `json:"select_all_types"`
+	ServiceLevel  *string          `json:"service_level"`
+	ServiceLevels *[]string        `json:"service_levels"`
+	SmartProxies  *[]orgSmartProxy `json:"smart_proxies"`
+	Subnets       *[]orgSubnet     `json:"subnets,omitempty"`
+	//SystemPurposes
+	Title     *string    `json:"title"`
+	UpdatedAt *string    `json:"updated_at"`
+	Users     *[]orgUser `json:"users"`
+}
+
+// OrganizationCreate defines model for OrganizationCreate.
+type OrganizationCreate struct {
+	Organization struct {
+		ComputeResourceIDs      *[]int  `json:"compute_resource_ids,omitempty"`
+		ConfigTemplateIDs       *[]int  `json:"config_template_ids,omitempty"`
+		Description             *string `json:"description,omitempty"`
+		DomainIDs               *[]int  `json:"domain_ids,omitempty"`
+		EnvironmentIDs          *[]int  `json:"environment_ids,omitempty"`
+		HostgroupIDs            *[]int  `json:"hostgroup_ids,omitempty"`
+		Label                   *string `json:"label,omitempty"`
+		MediumIDs               *[]int  `json:"medium_ids,omitempty"`
+		Name                    string  `json:"name"`
+		ProvisioningTemplateIDs *[]int  `json:"provisioning_template_ids,omitempty"`
+		PtableIDs               *[]int  `json:"ptable_ids,omitempty"`
+		RealmIds                *[]int  `json:"realm_ids,omitempty"`
+		SmartProxyIDs           *[]int  `json:"smart_proxy_ids,omitempty"`
+		SubnetIDs               *[]int  `json:"subnet_ids,omitempty"`
+		UserIDs                 *[]int  `json:"user_ids,omitempty"`
+	} `json:"organization"`
+}
+
+// OrganizationUpdate defines model for OrganizationUpdate.
+type OrganizationUpdate struct {
+	Organization struct {
+		ComputeResourceIDs      *[]int    `json:"compute_resource_ids,omitempty"`
+		ConfigTemplateIDs       *[]int    `json:"config_template_ids,omitempty"`
+		Description             *string   `json:"description,omitempty"`
+		DomainIDs               *[]int    `json:"domain_ids,omitempty"`
+		EnvironmentIDs          *[]int    `json:"environment_ids,omitempty"`
+		HostgroupIDs            *[]int    `json:"hostgroup_ids,omitempty"`
+		IgnoreTypes             *[]string `json:"ignore_types,omitempty"`
+		MediumIds               *[]int    `json:"medium_ids,omitempty"`
+		Name                    *string   `json:"name,omitempty"`
+		ParentID                *int      `json:"parent_id,omitempty"`
+		ProvisioningTemplateIDs *[]int    `json:"provisioning_template_ids,omitempty"`
+		PtableIDs               *[]int    `json:"ptable_ids,omitempty"`
+		RealmIDs                *[]int    `json:"realm_ids,omitempty"`
+		SmartProxyIDs           *[]int    `json:"smart_proxy_ids,omitempty"`
+		SubnetIDs               *[]int    `json:"subnet_ids,omitempty"`
+		UserIDs                 *[]int    `json:"user_ids,omitempty"`
+	} `json:"organization"`
+	RedhatRepositoryURL *string `json:"redhat_repository_url,omitempty"`
+}
+
+// Organizations is an interface for interacting with
+// Red Hat Satellite organizations
+type Organizations interface {
+	DeleteOrganization(ctx context.Context, orgID int) (*http.Response, error)
+	CreateOrganization(ctx context.Context, orgCreate OrganizationCreate) (*Organization, *http.Response, error)
+	GetOrganizationByID(ctx context.Context, orgID int) (*Organization, *http.Response, error)
+}
+
+// OrganizationsOp handles communication with the Organization related methods of the
+// Red Hat Satellite REST API
+type OrganizationsOp struct {
+	client *Client
+}
+
+// CreateOrganization creates a new organization
+func (s *OrganizationsOp) CreateOrganization(ctx context.Context, orgCreate OrganizationCreate) (*Organization, *http.Response, error) {
+	path := katelloBasePath + "/organizations"
+
+	req, err := s.client.NewRequest(ctx, http.MethodPost, path, orgCreate)
+	if err != nil {
+		return nil, nil, err
+	}
+	org := new(Organization)
+	resp, err := s.client.Do(ctx, req, org)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return org, resp, err
+}
+
+// DeleteOrganization deletes an organization by its ID
+func (s *OrganizationsOp) DeleteOrganization(ctx context.Context, orgID int) (*http.Response, error) {
+	path := katelloBasePath + "/organizations/" + strconv.Itoa(orgID)
+
+	req, err := s.client.NewRequest(ctx, http.MethodDelete, path, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := s.client.Do(ctx, req, nil)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, err
+}
+
+// GetOrganizationByID gets a single Organization by its ID
+func (s *OrganizationsOp) GetOrganizationByID(ctx context.Context, orgID int) (*Organization, *http.Response, error) {
+	path := katelloBasePath + "/organizations/" + strconv.Itoa(orgID)
+	req, err := s.client.NewRequest(ctx, http.MethodGet, path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	org := new(Organization)
+	resp, err := s.client.Do(ctx, req, org)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return org, resp, err
+}
+
+// UpdateOrganization the settings of an organization by its ID
+// func (s *OrganizationsOp) UpdateOrganization(ctx context.Context, orgID int, update *OrganizationUpdate) (*http.Response, error) {
+// 	path := katelloBasePath + "/organizations/" + strconv.Itoa(orgID)
+
+// 	req, err := s.client.NewRequest(ctx, http.MethodPut, path, update)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	root := new(registerClientRequestRoot)
+// 	resp, err := s.client.Do(ctx, req, root)
+// 	if err != nil {
+// 		return resp, err
+// 	}
+
+// 	return resp, err
+// }
